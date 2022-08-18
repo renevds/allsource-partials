e9cb3f064fa510174778fda7335aab19c71ca18f
renevds <rene.v.d.s.9@gmail.com>
2022-08-19 01:59:32 +0200

    Custom react version

---
+++ global/extensions/react-custom/index.ts
@@ -0,0 +1,5 @@
+import { ReactCustomAspect } from './react-custom.aspect';
+
+export type { ReactCustomMain } from './react-custom.main.runtime';
+export default ReactCustomAspect;
+export { ReactCustomAspect };
---
+++ global/extensions/react-custom/react-custom.aspect.ts
@@ -0,0 +1,5 @@
+import { Aspect } from '@teambit/harmony';
+
+export const ReactCustomAspect = Aspect.create({
+  id: 'allsource.global/extensions/react-custom',
+});
